b5132566461b4ad50771f71f23201756af4cf1b4
Rill <160830457+chappuis13@users.noreply.github.com>
2025-10-19 15:54:56 +0700

    Add files via upload

---
+++ Session 3/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-native-vector-icons/Ionicons' {
+  const content: any;
+  export default content;
+}
+
+declare module 'react-native-vector-icons/*' {
+  const content: any;
+  export default content;
+}
